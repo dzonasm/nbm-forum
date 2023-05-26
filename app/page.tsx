d2c9029094b6965888c7edc13e39b7cb937532b5
Jonas <jonas.marciulevicius@halopowered.com>
2023-05-26 04:48:13 +0300

    feat: add react query

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query/src/QueryClientProvider';
 import clsx from 'clsx';
 import styles from './page.module.scss';
 
+const queryClient = new QueryClient();
+
 export default function Home() {
   return (
-    <main className={clsx([styles.main])}>
-      <span
-        className={clsx(['nbm-helper-text', 'nbm-text--color-on-background'])}
-      >
-        hello nbm
-      </span>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className={clsx([styles.main])}>
+        <span
+          className={clsx(['nbm-helper-text', 'nbm-text--color-on-background'])}
+        >
+          hello nbm
+        </span>
+      </main>
+    </QueryClientProvider>
   );
 }
---
+++ http/queries/queries.ts
@@ -0,0 +1,12 @@
+// useQuery({
+//   queryKey: ['todos', todoId],
+//   queryFn: async () => {
+//     const response = await fetch('/todos/' + todoId);
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   },
+// });
+
+// export const getApiKey;
